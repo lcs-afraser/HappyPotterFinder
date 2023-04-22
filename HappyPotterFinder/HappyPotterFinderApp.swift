ada2f6675211c9a4cc4bc1dc2a83903bfdb87ea8
Alistair Fraser <afraser@lcs.on.ca>
2023-04-22 12:24:11 -0400

    Initial Commit

---
+++ HappyPotterFinder/HappyPotterFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyPotterFinderApp.swift
+//  HappyPotterFinder
+//
+//  Created by Alistair Fraser on 2023-04-22.
+//
+
+import SwiftUI
+
+@main
+struct HappyPotterFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
